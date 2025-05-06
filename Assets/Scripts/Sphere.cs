3d79c50f64471385722a41f29fac235e0f970950
John Alexander Ortiz <johnspartanunsc@gmail.com>
2025-05-06 12:22:12 -0500

    Cambio de Color

--- Assets/Scripts/Sphere.cs
+++ Assets/Scripts/Sphere.cs
@@ -1,11 +1,12 @@
 using UnityEngine;
 
 public class Sphere : MonoBehaviour
-{
+{    
+    public MeshRenderer meshRenderer;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        meshRenderer.material.color= Random.ColorHSV();
     }
 
     // Update is called once per frame
